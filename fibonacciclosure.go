08da49969c785263706229f68530dd4a2003e063
Martijn Vermaat <martijn@vermaat.name>
2016-03-07 07:57:41 +0100

    Shorter Fibonacci closure

--- fibonacciclosure.go
+++ fibonacciclosure.go
@@ -2,9 +2,8 @@ package tour
 
 func Fibonacci() func() int {
 	f0, f1 := -1, 1
-	return func() (f2 int) {
-		f2 = f0 + f1
-		f0, f1 = f1, f2
-		return
+	return func() int {
+		f0, f1 = f1, f0+f1
+		return f1
 	}
 }
